35cd23cc4846124916ec0396e8ceca1356473e79
gerrardmourao Mourão <gerardmo2022@gmail.com>
2024-05-29 23:50:38 +0000

    feat: instalação e implantação do react router-router-dom e do styled-componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
 
-export default function App() {
+import { Home } from "./pages/Home"
+
+import { theme } from "./styles/theme"
+
+const router = createBrowserRouter ([
+
+  {
+    path: "/",
+    element: <Home/>
+  }
+])
+
+export function App() {
   return (
-    <div>App</div>
+    <ThemeProvider theme={theme}>
+          <RouterProvider router ={router} />
+    </ThemeProvider>
   )
 }
